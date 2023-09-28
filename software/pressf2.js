06649f376a649c899bec72ee21f785a36abea819
Ertuğrul Çukuryurt <mecukuryurt@gmail.com>
2023-09-28 21:28:09 +0300

    Fixed the redirect error

--- software/pressf2.js
+++ software/pressf2.js
@@ -5,7 +5,7 @@ function logkey(e)
     if (e.code == "F2")
     {
         console.log("F2 pressed");
-        window.open('https://bun.filedit.ch/uzdoSioDSwBmWAAPmUx.mp3');
+        window.open('https://mecukuryurt.github.io/redirect/redirect.html');
     }
 
 } 
