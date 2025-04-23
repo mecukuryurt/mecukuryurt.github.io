c27860972ab79f6e699b884a498a7932fadee4ae
mecukuryurt <mecukuryurt@gmail.com>
2025-04-23 21:17:59 +0300

    edited code for common datetime label

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,13 @@ function myFunction(x) {
   document.getElementById("sidebarmenu").classList.toggle("sbmenuc");
 }
 
-document.onload = function() {}
+window.onload = function() {
+  document.getElementById("datenum").innerText = "23.04.2025";
+  document.getElementById("timenum").innerHTML = "211.10";
+  console.log("yay");
+}
+/*
+$(document).ready(function() {
+  $('#navigation').load('/pages/navigation.html');
+});
+*/
